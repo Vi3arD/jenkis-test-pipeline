f94de2f0e3a25b5859e1e3bf962054122117f50d
Kirill Antipov <k.antipov@haulmont.com>
2022-01-21 12:16:45 +0300

    first

--- src/com/haulmont/cloudcontrol/Utilities.groovy
+++ src/com/haulmont/cloudcontrol/Utilities.groovy
@@ -3,7 +3,6 @@ package com.haulmont.cloudcontrol
 class Utilities {
     static def mvn(script, args) {
         script.sh "echo something"
-        script.sh "echo ${script.tool 'Maven'}"
         script.sh "echo ${script.tool}"
     }
 }
