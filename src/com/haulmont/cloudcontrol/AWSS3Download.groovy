51af5e1cc0b195e9889c7b2fb7f81a8952f91db4
Kirill Antipov <k.antipov@haulmont.com>
2022-01-21 18:42:19 +0300

    first

--- src/com/haulmont/cloudcontrol/AWSS3Download.groovy
+++ src/com/haulmont/cloudcontrol/AWSS3Download.groovy
@@ -13,7 +13,7 @@ class AWSS3Download implements Action, Serializable {
 //        script.env.AWS_REGION = parameters[GlobalVars.AWS_REGION][0]
 //        script.env.AWS_ACCESS_KEY_ID = parameters[GlobalVars.AWS_ACCESS_KEY_ID][0]
 //        script.env.AWS_SECRET_ACCESS_KEY = parameters[GlobalVars.AWS_SECRET_ACCESS_KEY][0]
-        script.sh "aws s3 cp s3://${script.env[GlobalVars.BUCKET_NAME][0]}/terraform /data --recursive"
+        script.sh "aws s3 cp s3://${script.env[GlobalVars.BUCKET_NAME]}/terraform /data --recursive"
         script.sh "cd /data && ls -lsa"
     }
 
