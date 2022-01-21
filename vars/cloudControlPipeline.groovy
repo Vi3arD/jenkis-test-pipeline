0e76ffc7f1d3612c3cb493046ff11cda15497914
Kirill Antipov <k.antipov@haulmont.com>
2022-01-21 12:54:13 +0300

    first

--- vars/cloudControlPipeline.groovy
+++ vars/cloudControlPipeline.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 package vars
 
-import groovy.json.JsonSlurper
+import groovy.json.JsonSlurperClassic
 import com.haulmont.cloudcontrol.AWSS3Download
 import com.haulmont.cloudcontrol.GlobalVars
 
 def call(String request) {
-    def jsonSlurper = new JsonSlurper()
+    def jsonSlurper = new JsonSlurperClassic()
     def structure = jsonSlurper.parseText(request)
     AWSS3Download awss3Download = new AWSS3Download()
 //    echo structure[GlobalVars.ENV][GlobalVars.BUCKET_NAME]
