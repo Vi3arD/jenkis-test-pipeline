7dcc3ed4996ebea886cba060892b89de52a9ec59
Kirill Antipov <k.antipov@haulmont.com>
2022-01-21 12:22:55 +0300

    first

--- src/com/haulmont/cloudcontrol/Utilities.groovy
+++ src/com/haulmont/cloudcontrol/Utilities.groovy
@@ -4,6 +4,8 @@ class Utilities {
     static def mvn(script, args) {
         script.sh "echo something"
         script.sh "echo ${script}"
+        script.sh "echo pwd"
+        script.sh "pwd"
         script.sh "${script.tool 'terraform'} -version"
     }
 }
