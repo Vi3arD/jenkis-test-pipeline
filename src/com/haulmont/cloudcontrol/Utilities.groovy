419591a3b414b5e5a1cb3728fb3d1f785feb05f3
Kirill Antipov <k.antipov@haulmont.com>
2022-01-21 12:15:46 +0300

    first

---
+++ src/com/haulmont/cloudcontrol/Utilities.groovy
@@ -0,0 +1,9 @@
+package com.haulmont.cloudcontrol
+
+class Utilities {
+    static def mvn(script, args) {
+        script.sh "echo something"
+        script.sh "echo ${script.tool 'Maven'}"
+        script.sh "echo ${script.tool}"
+    }
+}
--- vars/cloudControlPipeline.groovy
+++ vars/cloudControlPipeline.groovy
@@ -4,6 +4,7 @@ package vars
 import groovy.json.JsonSlurper
 import com.haulmont.cloudcontrol.AWSS3Download
 import com.haulmont.cloudcontrol.GlobalVars
+import com.haulmont.cloudcontrol.Utilities
 
 def call(String request) {
 //    def jsonSlurper = new JsonSlurper()
@@ -11,6 +12,7 @@ def call(String request) {
     AWSS3Download awss3Download = new AWSS3Download()
 //    echo structure[GlobalVars.ENV][GlobalVars.BUCKET_NAME]
     container(awss3Download.getContainerName()) {
-        awss3Download.action(null)
+        Utilities.mvn(this, "trst")
+//        awss3Download.action(null)
     }
 }
