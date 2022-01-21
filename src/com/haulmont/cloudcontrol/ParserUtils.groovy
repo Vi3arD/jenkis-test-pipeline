1342f0802c713fdde8b2179ec408cf728ee99850
Kirill Antipov <k.antipov@haulmont.com>
2022-01-21 18:38:53 +0300

    first

--- src/com/haulmont/cloudcontrol/ParserUtils.groovy
+++ src/com/haulmont/cloudcontrol/ParserUtils.groovy
@@ -5,10 +5,7 @@ class ParserUtils {
     public static void toEnv(def script, def parameters) {
         script.sh "echo ${parameters}"
         parameters[0].each {
-            entry ->
-                script.sh "echo ${entry.key}"
-                script.env["${entry.key}"] = entry.value
-                script.env.$entry.key = entry.value
+            entry -> script.env["${entry.key}"] = entry.value
         }
     }
 
