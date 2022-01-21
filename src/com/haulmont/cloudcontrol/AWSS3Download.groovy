9c13012a27c58ef73939ce8e7353923db678856a
Kirill Antipov <k.antipov@haulmont.com>
2022-01-21 13:18:47 +0300

    first

--- src/com/haulmont/cloudcontrol/AWSS3Download.groovy
+++ src/com/haulmont/cloudcontrol/AWSS3Download.groovy
@@ -7,9 +7,9 @@ class AWSS3Download implements Action {
     @Override
     void action(script, parameters) {
         script.sh "export AWS_REGION=${parameters[GlobalVars.AWS_REGION][0]} " +
-                "&& export AWS_ACCESS_KEY_ID=${parameters[GlobalVars.AWS_ACCESS_KEY_ID] as String} " +
-                "&& export AWS_SECRET_ACCESS_KEY=${parameters[GlobalVars.AWS_SECRET_ACCESS_KEY as String]}"
-        script.sh "aws s3 cp s3://${parameters[GlobalVars.BUCKET_NAME] as String}/terraform /data --recursive"
+                "&& export AWS_ACCESS_KEY_ID=${parameters[GlobalVars.AWS_ACCESS_KEY_ID][0]} " +
+                "&& export AWS_SECRET_ACCESS_KEY=${parameters[GlobalVars.AWS_SECRET_ACCESS_KEY][0]}"
+        script.sh "aws s3 cp s3://${parameters[GlobalVars.BUCKET_NAME][0]}/terraform /data --recursive"
         script.sh "cd /data && ls -lsa"
     }
 
