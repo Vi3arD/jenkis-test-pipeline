3c3ff2b21bce6743bb1089879e57b05029068b5a
Kirill Antipov <k.antipov@haulmont.com>
2022-01-24 14:32:43 +0300

    first

---
+++ src/com/haulmont/cloudcontrol/Notifier.groovy
@@ -0,0 +1,13 @@
+package com.haulmont.cloudcontrol
+
+class Notifier {
+
+    static void send(def script, String type, String description = "") {
+//        def response = httpRequest consoleLogResponseBody: true,
+//                contentType: 'APPLICATION_JSON',
+//                httpMode: 'GET',
+//                customHeaders: [[name: 'X-Jenkins-Token', value: "${CLOUD_CONTROL_AUTH_TOKEN}"]],
+//                url: "${CALLBACK_URL}/rest/jenkins/${script.env[""]}?workspaceId=${WORKSPACE_ID}&deploymentId=${DEPLOYMENT_ID}", validResponseCodes: '200'
+    }
+
+}
--- vars/cloudControlPipeline.groovy
+++ vars/cloudControlPipeline.groovy
@@ -4,11 +4,13 @@ package vars
 import com.haulmont.cloudcontrol.ClassKeeper
 import com.haulmont.cloudcontrol.Utils
 import com.haulmont.cloudcontrol.GlobalVars
+import com.haulmont.cloudcontrol.Notifier
 
 def call(String request) {
     def structure = readJSON text: request, returnPojo: true
     Utils.toEnv(this, structure[GlobalVars.ENV])
 
+    String flowStatus = "success"
     int currentStep
     int size = structure[GlobalVars.ACTIONS].size()
     Map beans = ClassKeeper.getBeans()
@@ -16,27 +18,36 @@ def call(String request) {
     if (GlobalVars.INSTALL.equals(structure[GlobalVars.TYPE])) {
         try {
             for (currentStep = 0; currentStep < size; currentStep++) {
-                def executor = beans.get(structure[GlobalVars.ACTIONS][currentStep][GlobalVars.EXECUTOR])
-                container(executor.getContainerName()) {
+                def action = structure[GlobalVars.ACTIONS][currentStep]
+                def executor = beans.get(action[GlobalVars.EXECUTOR])
+//                container(executor.getContainerName()) {
                     executor.action(this)
-                }
+//                }
+                Notifier.send(this, "status", action[GlobalVars.NAME] as String)
             }
         } catch (Exception e) {
             for (currentStep; currentStep >= 0; currentStep--) {
-                def executor = beans.get(structure[GlobalVars.ACTIONS][currentStep][GlobalVars.EXECUTOR])
+                def action = structure[GlobalVars.ACTIONS][currentStep]
+                def executor = beans.get(action[GlobalVars.EXECUTOR])
                 container(executor.getContainerName()) {
                     executor.rollback(this)
                 }
+                Notifier.send(this, "status", action[GlobalVars.NAME] as String)
             }
+            flowStatus = "failed"
             echo "error -> ${e}"
         }
     } else if (GlobalVars.DESTROY.equals(structure[GlobalVars.TYPE])) {
         for (currentStep = size - 1; currentStep >= 0; currentStep--) {
-            def executor = beans.get(structure[GlobalVars.ACTIONS][currentStep][GlobalVars.EXECUTOR])
+            def action = structure[GlobalVars.ACTIONS][currentStep]
+            def executor = beans.get(action[GlobalVars.EXECUTOR])
             container(executor.getContainerName()) {
                 executor.rollback(this)
             }
+            Notifier.send(this, "status", action[GlobalVars.NAME] as String)
         }
     }
+
+    Notifier.send(this, flowStatus)
 
 }
