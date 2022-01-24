d9ab8a54bff62ac55d300fc075aa14fac8d3fe6b
Kirill Antipov <k.antipov@haulmont.com>
2022-01-24 18:24:44 +0300

    first

--- vars/poder.groovy
+++ vars/poder.groovy
@@ -1,41 +1,7 @@
 #!/usr/bin/env groovy
 package vars
 
-def String pod() {
-    def podTemplate = '''
-apiVersion: v1
-kind: Pod
-spec:
-  containers:
-    - name: terraform
-      image: hashicorp/terraform:1.0.6
-      command:
-        - sleep
-      args:
-        - "99d"
-      volumeMounts:
-        - name: data
-          mountPath: /data
-    - name: ansible
-      image: ansible/ansible-runner:1.4.7
-      command:
-        - sleep
-      args:
-        - "99d"
-      volumeMounts:
-        - name: data
-          mountPath: /data
-    - name: aws
-      image: amazon/aws-cli:2.4.12
-      command:
-        - sleep
-      args:
-        - "99d"
-      volumeMounts:
-        - name: data
-          mountPath: /data
-  volumes:
-    - name: data
-      emptyDir: {}'''
-    return podTemplate
+def call() {
+    [containerTemplate(name: 'terraform', image: 'hashicorp/terraform:1.0.6', command: 'sleep', args: '99d'),
+    containerTemplate(name: 'aws', image: 'amazon/aws-cli:2.4.12', command: 'sleep', args: '99d')]
 }
