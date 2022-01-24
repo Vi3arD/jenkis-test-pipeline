ca6df085d4ad564e52c750e2b94e03afd99f39cc
Kirill Antipov <k.antipov@haulmont.com>
2022-01-24 14:34:22 +0300

    first

--- src/com/haulmont/cloudcontrol/actions/AWSS3Download.groovy
+++ src/com/haulmont/cloudcontrol/actions/AWSS3Download.groovy
@@ -8,10 +8,9 @@ class AWSS3Download implements Action, Serializable {
 
     @Override
     void action(def script) {
-//        script.sh "aws s3 cp s3://${script.env[GlobalVars.BUCKET_NAME]}/terraform /data --recursive"
-//        script.sh "cd /data && ls -lsa"
-
         script.container(CONTAINER) {
+            script.sh "aws s3 cp s3://${script.env[GlobalVars.BUCKET_NAME]}/terraform /data --recursive"
+            script.sh "cd /data && ls -lsa"
             script.sh "echo AWSS3Download"
             script.sh "echo AWSS3Download"
             script.sh "echo AWSS3Download"
